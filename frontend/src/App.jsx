643e50c822daf5d786c3909a9a55405aa9c6f09c
Владимир <2007.vofka@gmail.com>
2026-05-28 22:09:26 +0300

    add dashboard to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,20 +16,55 @@ import {
 
 } from "@/components/ui/input-otp";
 
-function App() {
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
 
-  const [count, setCount] = useState(0);
-
+export default function Page() {
   return (
-
-     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
-      </div>
-    </div>
-
-  );
-
+ 
+//  <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+//       <div className="w-full max-w-sm">
+//         {/* <LoginForm /> */}
+//       </div>
+//     </div>
+ 
+     <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbPage>October 2024</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-5">
+            {Array.from({ length: 20 }).map((_, i) => (
+              <div key={i} className="aspect-square rounded-xl bg-muted/50" />
+            ))}
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
 
-export default App;
